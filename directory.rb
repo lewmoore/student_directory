d07669895576089d444fa27b514f23b03c372dca
lewismoore <lewis.moore2707@gmail.com>
2017-12-09 22:05:41 +0000

    Added user input

--- directory.rb
+++ directory.rb
@@ -11,6 +11,24 @@ students =[
   {name: "Joffrey Baratheon", cohort: :november},
   {name: "Norman Bates", cohort: :november}
 ]
+
+def input_students
+  puts "Please enter the name of the students"
+  puts "To finish, press return twice"
+
+  students = []
+
+  name = gets.chomp
+
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
+
 def print_header
   puts "The students of Villains Academy"
   puts "----------"
@@ -26,6 +44,7 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
