8af3463b1584545ef5eb6628890c3a6b2125f0d2
lewismoore <lewis.moore2707@gmail.com>
2017-12-09 23:32:43 +0000

    Added center (q6)

--- directory.rb
+++ directory.rb
@@ -1,4 +1,5 @@
 #5. Our code only works with the student name and cohort. Add more information: hobbies, country of birth, height, etc.
+#6. Research how the method center() of the String class works. Use it in your code to make the output beautifully aligned.
 
 
 def input_students
@@ -22,20 +23,20 @@ end
 
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "----------"
+  puts "The students of Villains Academy".center(100)
+  puts "----------".center(100)
 end
 
 def print(students)
   x = 0
   while x < students.length
-    puts "#{students[x][:name]}, #{students[x][:location]}, #{students[x][:hobbies]}, (#{students[x][:cohort]} cohort)"
+    puts "#{students[x][:name]}, #{students[x][:location]}, #{students[x][:hobbies]}, (#{students[x][:cohort]} cohort)".center(100)
     x += 1
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(100)
 end
 
 students = input_students
