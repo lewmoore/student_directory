4250fb13f4664f5041e5564db292fe29ec5f3305
lewismoore <lewis.moore2707@gmail.com>
2017-12-09 21:46:52 +0000

    Added code to methods

--- directory.rb
+++ directory.rb
@@ -11,10 +11,19 @@ students =[
   "Joffrey Baratheon",
   "Norman Bates"
 ]
+def print_header
+  puts "The students of Villains Academy"
+  puts "----------"
+end
 
-puts "The students of Villains Academy"
-puts "----------"
+def print(names)
+  names.each {|name| puts name}
+end
 
-students.each {|name| puts name}
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
 
-puts "Overall, we have #{students.count} great students"
+print_header
+print(students)
+print_footer(students)
