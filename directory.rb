53e1bd0e6c4911721d04f6e1e3cfaedfd7459cac
lewismoore <lewis.moore2707@gmail.com>
2017-12-09 23:37:06 +0000

    Added changeable cohort w/ defaiult value

--- directory.rb
+++ directory.rb
@@ -1,6 +1,7 @@
 #5. Our code only works with the student name and cohort. Add more information: hobbies, country of birth, height, etc.
 #6. Research how the method center() of the String class works. Use it in your code to make the output beautifully aligned.
-
+#7. In the input_students method the cohort value is hard-coded. How can you ask for both the name and the cohort?
+#7. What if one of the values is empty? Can you supply a default value?
 
 def input_students
   puts "Please enter the name of the students"
@@ -10,11 +11,14 @@ def input_students
   name = gets.chomp
 
   while !name.empty? do
+    puts "What cohort is this student in?"
+    cohort = gets.chomp
+    if cohort.empty? then cohort = "December" end
     puts "Where is this student from?"
     location = gets.chomp
     puts "Enter some hobbies for this student:"
     hobbies = gets.chomp
-    students << {name: name, cohort: :november, location: location, hobbies: hobbies}
+    students << {name: name, cohort: cohort, location: location, hobbies: hobbies}
     puts "Now we have #{students.count} students"
     name = gets.chomp
   end
