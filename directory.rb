bdc76086ff9c2c65c21eaabaf7cac02cce419e6f
lewismoore <lewis.moore2707@gmail.com>
2017-12-12 21:42:09 +0000

    Added Interactive Menu

--- directory.rb
+++ directory.rb
@@ -31,6 +31,28 @@ def print_header
   puts "----------".center(100)
 end
 
+def menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean"
+    end
+  end
+end
+
 def print(students)
   students.each { |name| puts "#{name[:name]}, #{name[:cohort]}, #{name[:location]}, #{name[:hobbies]}" }
 end
@@ -53,5 +75,6 @@ def print_footer(students)
     end
 end
 
-students = input_students
+
+menu
 no_students(students)
